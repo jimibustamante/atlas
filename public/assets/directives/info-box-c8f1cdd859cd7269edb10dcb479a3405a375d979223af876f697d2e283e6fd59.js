f4eb69a184e639b3cd8c8ea81987daf62e7455e0
Ubuntu <ubuntu@ip-172-31-20-91.us-west-2.compute.internal>
2015-12-14 02:11:40 +0000

    Gemfile.lock

---
+++ public/assets/directives/info-box-c8f1cdd859cd7269edb10dcb479a3405a375d979223af876f697d2e283e6fd59.js
@@ -0,0 +1,1 @@
+angular.module("atlasApp").directive("infoBox",["filtersFact",function(filtersFact){function link(scope,element,attrs){}return{templateUrl:"/assets/templates/info-box.html",link:link}}]);
