e8f86cdba238f655b47dc500c7aadf6fd6c749dc
jimi <jimibustamante@gmail.com>
2015-12-21 13:13:51 -0300

    Cambio de base de datos a mysql y cambios esteticos

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
-# atlas
+## Synopsis
+
+Este repositorio contiene el proyecto web 'Atlas de Depósitos Relaves'. Mediante él un usuario podrá efectuar una búsqueda basada en región/comuna o dueño/faena, la que entregará como resultado la posición en el mapa y la información de cada depósito.
+
+El proyecto está escrito en Ruby y Javascript. El servidor Ruby on Rails, se encarga de servir los assets necesarios para el cliente y funciona como API. Por el lado del cliente, la app web está construida en AngulajJS.  
+
+
+## Prerquisitos
+  
+Se debe tener instalado git
+
+```sh
+$ sudo yum update
+$ sudo yum install git
+```
+
+Además, hay que instalar [rvm], que se encargará de instalar Ruby.
+
+```sh
+$ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+$ sudo yum install curl
+$ curl -L get.rvm.io | bash -s stable
+
+# Si corriste el instalador como sudo, ejecuta:
+$ source /usr/local/rvm/rvm.sh
+# Si lo instalaste mediante una cuenta de usuario con acceso sudo:
+$ source ~/.rvm/rvm.sh
+```
+Para la verificación de las dependencias de [rvm], ejecuta:
+```sh
+$ rvm requirements
+```
+Este comando identificará qué dependencias necesitas instalar y te mostrará cómo hacerlo.
+
+Ya con [rvm] listo, hay que instalar Ruby. La versión que se está usando es la 2.0.0-p643, por lo que hay que correr lo siguiente:
+```sh
+$ rvm install ruby-2.0.0-p643
+# y luego dejar esta versión como por defecto
+$ rvm use ruby-2.0.0-p643 --default
+```
+
+Para la instalación de las gemas que utiliza el proyecto utilizamos [bundler]. Pa su instalación:
+```sh
+# Dependencias
+$ yum install gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel ruby-devel libxml2 libxml2-devel libxslt libxslt-devel
+# Luego se instala la gema
+$ gem install bundler
+```
+
+
+## Instalación
+
+El proyecto utiliza una seria de gemas para su funcionamiento. Estas se encuentran listadas el archivo Gemfile. Para su instalación utilizamos [bundler] corriendo lo siguiente:
+```sh
+$ bundle install
+```
+
+
+
+```sh
+$ git clone [git-repo-url] dillinger
+$ cd dillinger
+$ npm i -d
+$ mkdir -p downloads/files/{md,html,pdf}
+$ gulp build --prod
+$ NODE_ENV=production node app
+```
+
+## API Reference
+
+Depending on the size of the project, if it is small and simple enough the reference docs can be added to the README. For medium size to larger projects it is important to at least provide a link to where the API reference docs live.
+
+## Tests
+
+Describe and show how to run the tests with code examples.
+
+## Contributors
+
+Let people know how they can dive into the project, include important links to things like issue trackers, irc, twitter accounts if applicable.
+
+## License
+
+A short snippet describing the license (MIT, Apache, etc.)
--- db/schema.rb
+++ db/schema.rb
@@ -13,50 +13,47 @@
 
 ActiveRecord::Schema.define(version: 20151214195327) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "communes", force: :cascade do |t|
-    t.integer  "province_id", null: false
-    t.string   "name",        null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "province_id", limit: 4,   null: false
+    t.string   "name",        limit: 255, null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
   end
 
   create_table "labors", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.integer  "owner_id",   null: false
-    t.integer  "commune_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255, null: false
+    t.integer  "owner_id",   limit: 4,   null: false
+    t.integer  "commune_id", limit: 4,   null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   add_index "labors", ["commune_id"], name: "index_labors_on_commune_id", using: :btree
   add_index "labors", ["owner_id"], name: "index_labors_on_owner_id", using: :btree
 
   create_table "mining_wastes", force: :cascade do |t|
-    t.integer  "sernageomin_id",                                     null: false
-    t.string   "status",                                             null: false
-    t.string   "name"
-    t.decimal  "height",                    precision: 10, scale: 6
-    t.string   "utm_north"
-    t.string   "utm_east"
-    t.decimal  "lon",                       precision: 10, scale: 6
-    t.decimal  "lat",                       precision: 10, scale: 6
-    t.integer  "labor_id",                                           null: false
-    t.integer  "region_id",                                          null: false
-    t.datetime "created_at",                                         null: false
-    t.datetime "updated_at",                                         null: false
-    t.string   "mining_waste_type"
-    t.string   "partial_volume"
-    t.string   "total_volume"
-    t.string   "partial_ton"
-    t.string   "total_ton"
-    t.string   "resolution"
+    t.integer  "sernageomin_id",            limit: 4,                            null: false
+    t.string   "status",                    limit: 255,                          null: false
+    t.string   "name",                      limit: 255
+    t.decimal  "height",                                precision: 10, scale: 6
+    t.string   "utm_north",                 limit: 255
+    t.string   "utm_east",                  limit: 255
+    t.decimal  "lon",                                   precision: 10, scale: 6
+    t.decimal  "lat",                                   precision: 10, scale: 6
+    t.integer  "labor_id",                  limit: 4,                            null: false
+    t.integer  "region_id",                 limit: 4,                            null: false
+    t.datetime "created_at",                                                     null: false
+    t.datetime "updated_at",                                                     null: false
+    t.string   "mining_waste_type",         limit: 255
+    t.string   "partial_volume",            limit: 255
+    t.string   "total_volume",              limit: 255
+    t.string   "partial_ton",               limit: 255
+    t.string   "total_ton",                 limit: 255
+    t.string   "resolution",                limit: 255
     t.date     "resolution_date"
-    t.string   "resolution_number"
+    t.string   "resolution_number",         limit: 255
     t.date     "resolution_closure_date"
-    t.string   "resolution_closure_number"
+    t.string   "resolution_closure_number", limit: 255
   end
 
   add_index "mining_wastes", ["labor_id"], name: "index_mining_wastes_on_labor_id", using: :btree
@@ -64,23 +61,23 @@ ActiveRecord::Schema.define(version: 20151214195327) do
   add_index "mining_wastes", ["sernageomin_id"], name: "index_mining_wastes_on_sernageomin_id", using: :btree
 
   create_table "owners", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "provinces", force: :cascade do |t|
-    t.string   "name",       null: false
-    t.integer  "region_id",  null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "name",       limit: 255, null: false
+    t.integer  "region_id",  limit: 4,   null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "regions", force: :cascade do |t|
-    t.string   "name",          null: false
-    t.string   "region_number", null: false
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.string   "name",          limit: 255, null: false
+    t.string   "region_number", limit: 255, null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
